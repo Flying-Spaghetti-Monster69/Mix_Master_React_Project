2c2829e2ff2bddc3849072db83f19163febad87c
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-04-27 12:55:27 -0500

    newsletter structure

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
 const Newsletter = () => {
-  return <div>newsletter</div>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      {}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="Felipe"
+        />
+      </div>
+      {}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          Last Name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="Garzon"
+        />
+      </div>
+      {}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default Newsletter;
