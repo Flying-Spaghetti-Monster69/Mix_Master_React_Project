698dc6cc3025da7c9903242ef0f3d5fc3569e701
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-04-26 22:08:28 -0500

    error page added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
 
 const Error = () => {
-  return <div>error</div>;
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Oooops!</h3>
+          <p>we can't seem to find the page you are looking for :C</p>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div>
+        <h3>something went wrong... :C</h3>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
