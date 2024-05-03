fa750e9b6de65565bb50785a1cc6a42311234e69
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-05-02 22:03:07 -0500

    deployment ready

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import { loader as singleCocktailLoader } from "./pages/Cocktail";
 import { action as newsletterAction } from "./pages/Newsletter";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -59,7 +58,6 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>;
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
